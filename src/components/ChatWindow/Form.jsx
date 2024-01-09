acf34622c576fce7288dae0370e185ebdc7b841f
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2024-01-10 02:55:04 +0530

    further re-structuring of code; optimized re-rendering by Moving State Down

--- src/components/ChatWindow/Form.jsx
+++ src/components/ChatWindow/Form.jsx
@@ -4,6 +4,7 @@ import socket from '../../socket';
 import { addMessage } from '../../features/chatSlice/chatSlice';
 import { setInput } from '../../features/chatSlice/chatSlice';
 import { Box, Button } from '@mui/material';
+import { useCallback } from 'react';
 
 const Form = () => {
   const { username: senderUsername, room } = useSelector((state) => state.game);
@@ -11,7 +12,7 @@ const Form = () => {
 
   const dispatch = useDispatch();
 
-  const onClickSend = () => {
+  const onClickSend = useCallback(() => {
     socket.emit(
       'message',
       {
@@ -20,13 +21,13 @@ const Form = () => {
         roomId: room,
       },
       (r) => {
-        if (r.error) return console.log(r.m); // if an error is returned in the response set roomError to the error message and exit
+        if (r.error) return console.log(r.m);
         dispatch(addMessage(r));
       }
     );
 
     dispatch(setInput(''));
-  };
+  }, [dispatch, input, senderUsername, room]);
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {

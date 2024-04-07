3b6192cd0716ed8d5be435a41ea839aa54397b28
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2024-04-07 22:36:55 +0530

    added env

--- src/features/players/playersApiSlice.jsx
+++ src/features/players/playersApiSlice.jsx
@@ -6,13 +6,13 @@ export const playersApiSlice = apiSlice.injectEndpoints({
 		searchPlayers: builder.query({
 			query: (args) => {
 				const { username, limit } = args;
-				return { url: `/api/search?username=${username}&limit=${limit}` };
+				return { url: `${import.meta.env.VITE_REACT_APP_SEARCH_PLAYERS}?username=${username}&limit=${limit}` };
 			},
 		}),
 
 		getAllFriendsStatus: builder.query({
 			query: ({ username }) => {
-				return { url: `/api/friend/status?sender_username=${username}` };
+				return { url: `${import.meta.env.VITE_REACT_APP_FRIEND_STATUS}?sender_username=${username}` };
 			},
 			// Provides a list of `Posts` by `_id`.
 			// If any mutation is executed that `invalidate`s any of these tags, this query will re-run to be always up-to-date.
@@ -28,7 +28,7 @@ export const playersApiSlice = apiSlice.injectEndpoints({
 
 		sendFriendRequest: builder.mutation({
 			query: ({ senderUsername, receiverUsername }) => ({
-				url: `/api/friend/send_request?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
+				url: `${import.meta.env.VITE_REACT_APP_SEND_REQUEST}?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
 				method: 'POST',
 			}),
 			// Invalidates all Post-type queries providing the `LIST` id - after all, depending of the sort order,
@@ -38,7 +38,7 @@ export const playersApiSlice = apiSlice.injectEndpoints({
 
 		acceptFriendRequest: builder.mutation({
 			query: ({ senderUsername, receiverUsername }) => ({
-				url: `/api/friend/accept_request?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
+				url: `${import.meta.env.VITE_REACT_APP_ACCEPT_REQUEST}?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
 				method: 'PUT',
 			}),
 			// Invalidates all Post-type queries providing the `LIST` id - after all, depending of the sort order,
@@ -48,7 +48,7 @@ export const playersApiSlice = apiSlice.injectEndpoints({
 
 		deleteFriend: builder.mutation({
 			query: ({ senderUsername, receiverUsername }) => ({
-				url: `/api/friend/unfriend?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
+				url: `${import.meta.env.VITE_REACT_APP_DELETE_FRIEND}?sender_username=${senderUsername}&receiver_username=${receiverUsername}`,
 				method: 'DELETE',
 			}),
 			// Invalidates all Post-type queries providing the `LIST` id - after all, depending of the sort order,
